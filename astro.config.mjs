e13bf949e61e5a063036cf77a8fbf13a2fa8182f
AstridNielsen-lab <juliocamposmachado@gmail.com>
2025-01-06 23:34:34 -0300

    Atualizando Blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://juliocamposmachado.com.br',
+	integrations: [
+		mdx(),
+		sitemap({
+			changefreq: 'weekly',
+			priority: 0.7,
+			lastmod: new Date()
+		})
+	],
+	markdown: {
+		drafts: true,
+		shikiConfig: {
+			theme: 'github-dark',
+			wrap: true
+		}
+	}
 });
