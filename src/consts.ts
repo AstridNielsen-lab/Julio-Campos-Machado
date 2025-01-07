e2884f82c48582136c0b98e799e2197d7015e860
Astrid Nielsen Labs <32886080+AstridNielsen-lab@users.noreply.github.com>
2025-01-07 03:03:50 -0300

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Julio Campos Machado - Portfolio';
+export const SITE_TITLE = 'Portfolio';
 export const SITE_DESCRIPTION = 'Locutor da Rádio Tatuapé FM e Fundador da Like Look Solutions';
